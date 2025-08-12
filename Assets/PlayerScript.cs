862fb423208e862248c0c48bd76bb97b80b00daa
AveryS-Jr <averysimpson.ga@gmail.com>
2025-08-12 16:10:22 -0400

    A lot of stuff

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Vector3 up;
+    private Vector3 left;
+
+    public GameObject gameManagerObject;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletSpeed = 5f;
+
+    public float bulletTimer;
+
+    public Sprite one;
+    public Sprite two;
+
+    private SpriteRenderer s;
+
+    public int ammo;
+    public int maxAmmo;
+
+    public int xp = 0;
+
+    public int level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GetComponent<SpriteRenderer>();
+        up = new Vector3(0, 0.05f, 0);
+        left = new Vector3(0.05f, 0f, 0);
+        ammo = 15;
+        maxAmmo = 45;
+        level = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += up;
+            s.sprite = one;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= up;
+            s.sprite = two;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += left;
+        }
+        if(ammo == 0 && maxAmmo > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                if (level == 1)
+                {
+                    maxAmmo -= 15;
+                    ammo += 15;
+                }
+                else if (level == 2)
+                {
+                    maxAmmo -= 25;
+                    ammo += 25;
+                }
+                else if (level == 3)
+                {
+                    maxAmmo -= 30;
+                    ammo += 30;
+                }
+            }
+        }
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = 0f; 
+
+        Vector2 aimDirection = (mouseWorldPos - firePoint.position).normalized;
+
+        if (Input.GetMouseButtonDown(0) && ammo > 0)
+        {
+            Shoot(aimDirection);
+            ammo--;
+        }
+
+        if(level == 1 && xp == 15)
+        {
+            maxAmmo = 75;
+            ammo = 25;
+            level = 2;
+            xp = 0;
+        }
+
+        if (level == 2 && xp == 30)
+        {
+            maxAmmo = 90;
+            ammo = 30;
+            level = 3;
+            xp = 0;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+
+        }
+    }
+
+    void Shoot(Vector2 direction)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = direction * bulletSpeed;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Powerup" && level == 1)
+        {
+            ammo = 15;
+            maxAmmo = 45;
+        }
+
+        if (collision.gameObject.tag == "Powerup" && level == 2)
+        {
+            ammo = 25;
+            maxAmmo = 75;
+        }
+
+        if (collision.gameObject.tag == "Powerup" && level == 3)
+        {
+            ammo = 30;
+            maxAmmo = 90;
+        }
+
+        if (collision.gameObject.tag == "XP")
+        {
+            xp++;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "ammo")
+        {
+            maxAmmo += maxAmmo / 3;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Time")
+        {
+            gameManagerObject.GetComponent<gameManagerScript>().gameTimer += 15f;
+            Destroy(collision.gameObject);
+        }
+    }
+}
